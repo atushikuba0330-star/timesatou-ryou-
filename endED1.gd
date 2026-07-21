7cca8eb03523bf8e5f50223fb1415c0d6c71598d
migeru-ainn <w4vbd7q8ht@privaterelay.appleid.com>
2026-07-21 16:15:03 +0900

    ゲーム終了ボタンが機能するようになった

--- endED1.gd
+++ endED1.gd
@@ -1,9 +1,1 @@
 extends Control
-
-func _on_end_pressed() -> void:
-	pass # Replace with function body.
-	get_tree().quit()
-
-
-func _on_button_2_pressed() -> void:
-	pass # Replace with function body.
