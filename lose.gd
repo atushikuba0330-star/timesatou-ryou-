8c0eb0b93ea0ec6e54ee6d5252f50d09c6d06aad
migeru-ainn <w4vbd7q8ht@privaterelay.appleid.com>
2026-08-26 15:17:43 +0900

    敗北時と勝利時のSE追加

--- lose.gd
+++ lose.gd
@@ -1,7 +1,7 @@
 extends Control
 
 func _ready() -> void:
-	SePlayer.play_se("res://SE (1).wav")
+	SePlayer.play_se("res://audiostock_1511517.mp3")
 
 func _on_button_pressed():
 	get_tree().change_scene_to_file("res://main.tscn")
