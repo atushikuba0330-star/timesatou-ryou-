564a14ad831c9aac6983a5dd6675c917298da74c
Tanuki1003 <tanuking20@gmail.com>
2026-09-02 10:24:15 +0900

    effect（炎属性）

--- Assets/Effect Animation/fireshoottest.gd
+++ Assets/Effect Animation/fireshoottest.gd
@@ -1,31 +1,31 @@
 extends Sprite2D
 
-func launch(start_pos, end_pos):
+@export var move_time := 0.5
 
-	global_position = start_pos
-
+func launch(start_pos: Vector2, end_pos: Vector2):
+	global_position = start_pos + Vector2(100, 0)
 	frame = 0
 
-	_play_animation()
+	# フレームアニメーションを並列実行
+	_play_animation.call_deferred()
 
+	# 移動
 	var tween = create_tween()
-
 	tween.tween_property(
 		self,
 		"global_position",
-		end_pos,
-		0.5
+		end_pos + Vector2(100, 0), # 着弾地点も右へ50px,
+		move_time
 	)
 
 	await tween.finished
-
 	queue_free()
 
 
 func _play_animation():
+	for loop_count in range(4): # 4回ループ
+		for i in range(hframes):
+			frame = i
+			await get_tree().create_timer(0.05).timeout
 
-	for i in range(hframes):
-
-		frame = i
-
-		await get_tree().create_timer(0.05).timeout
+	frame = 0
