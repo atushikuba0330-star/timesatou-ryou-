5ae439c09a396a36e98100bb952c4f241fdeeccd
migeru-ainn <w4vbd7q8ht@privaterelay.appleid.com>
2026-09-08 14:09:26 +0900

    メニュー画面にキャラクター立ち絵配置しました

---
+++ menu.gd
@@ -0,0 +1,26 @@
+extends Node2D
+
+@onready var char_holder = $CharacterHolder
+
+var character_scenes = [
+	preload("res://CharaFire.tscn"),
+	preload("res://CharaWater.tscn"),
+	preload("res://CharaLight.tscn"),
+	preload("res://CharaThunder.tscn"),
+	preload("res://CharaDarkness.tscn")
+]
+
+# Called when the node enters the scene tree for the first time.
+func _ready() :
+	_show_random_character()
+
+func _show_random_character():
+	for child in char_holder.get_children():
+		child.queue_free()
+
+	var random_scene = character_scenes[randi() % character_scenes.size()]
+	var char_instance = random_scene.instantiate()
+	char_holder.add_child(char_instance)
+
+	char_instance.position = Vector2(1000, 100)
+	char_instance.scale = Vector2(1.0, 1.0)
