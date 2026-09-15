549a017b17047208605db4387d6e7be62a63f950
siranui0618-hue <siranui0618@gmail.com>
2026-09-15 22:08:42 +0900

    闇属性エフェクト追加及びフレテキ追加

---
+++ Assets/Effect Animation/darkmagic/dark_offset_2.gd
@@ -0,0 +1,23 @@
+extends Sprite2D
+
+func launch(start_pos, end_pos):
+	
+	await get_tree().create_timer(1).timeout
+
+	# 対面スロットに表示
+	global_position = end_pos+ Vector2(150, 0)
+
+	frame = 0
+
+	await _play_animation()
+
+	queue_free()
+
+
+func _play_animation():
+
+	for i in range(hframes * vframes):
+
+		frame = i
+
+		await get_tree().create_timer(0.05).timeout
