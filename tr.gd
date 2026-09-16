23dbda49efb114aad6093b17014937e660640cec
migeru-ainn <w4vbd7q8ht@privaterelay.appleid.com>
2026-09-16 14:22:43 +0900

    UIノ調整トストーリーモードノボタンヲ削除シタ

--- tr.gd
+++ tr.gd
@@ -1,6 +1,20 @@
 extends Button
 
+@onready var desc_label = $"../Label"
+@export var description_text: String = "" 
+
+func _ready():
+	connect("mouse_entered", Callable(self, "_on_mouse_entered"))
+	connect("mouse_exited", Callable(self, "_on_mouse_exited"))
+
 func _on_pressed() -> void:
 	pass # Replace with function body.
 	get_tree().change_scene_to_file("res://TR.START.tscn")
 	SePlayer.play_se("res://audiostock_60330.mp3")
+
+func _on_mouse_entered():
+	desc_label.text = description_text
+	desc_label.visible = true
+
+func _on_mouse_exited():
+	desc_label.visible = false
